cb565c0f97356d2e70548bcf32765930cdd9c1d3
U-K-06 <utkarsh7khajuria8@gmail.com>
2025-12-24 03:26:08 +0530

    added enemies and shoot and gui

---
+++ Character.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Data;
+using Game;
+using Raylib_cs;
+
+namespace Game
+{
+    class Character
+    {
+        private int speed;
+        private int damage;
+        private int size;
+        int posX;
+        int posY;
+        int health;
+        private Texture2D texture;
+        private Image image;
+        public void InitPlayer(int size, int damage, int speed, string filepath, int x, int y)
+        {
+            this.size = size;
+            this.damage = damage;
+            this.speed = speed;
+            this.posX = x;
+            this.posY = y;
+            image = Raylib.LoadImage(filepath);
+            Raylib.ImageResize(ref image, size, size);
+            texture = Raylib.LoadTextureFromImage(image);
+            Raylib.UnloadImage(image);
+            Console.WriteLine("Player created succesfuly");
+        }
+        public int GetSize()
+        {
+            return size;
+        }
+        public int GetDamage()
+        {
+            return damage;
+        }
+        public int GetSpeed()
+        {
+            return speed;
+        }
+        public int GetPosX()
+        { return posX; }
+
+        public int GetPosY() { return posY; }
+
+        public void SetSpeed(int speed)
+        {
+            this.speed = speed;
+        }
+        public void SetDamage(int damage)
+        {
+            this.damage = damage;
+        }
+        public void UpdatePosition(int x, int y)
+        {
+            posX = Math.Clamp(x, 0, 2880 - GetSize());
+            posY = Math.Clamp(y, 0, 1800 - GetSize());
+        }
+        public void DrawCharacter()
+        {
+            Raylib.DrawTexture(texture, posX, posY, Color.White);
+        }
+        ~Character()
+        {
+            Raylib.UnloadTexture(texture);
+        }
+    }
+    class Player : Character
+    {
+        public void HandleInput()
+        {
+            if (Raylib.IsKeyDown(KeyboardKey.Left) || Raylib.IsKeyDown(KeyboardKey.A))
+            {
+                UpdatePosition(GetPosX() - GetSpeed(), GetPosY());
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.Right) || Raylib.IsKeyDown(KeyboardKey.D))
+            {
+                UpdatePosition(GetPosX() + GetSpeed(), GetPosY());
+            }
+        }
+    }
+    class Bullet
+    {
+        private int speed = 5;
+        private string sprite = "|";
+        private int posX;
+        private int posY;
+        private Character player;
+        public Bullet(Character character)
+        {
+            player = character;
+            posX = character.GetPosX();
+            posY = character.GetPosY()-30;
+            speed = (character is Player) ? 10 : 5;
+        }
+        public int GetSpeed()
+        {
+            return speed;
+        }
+        public void SetSpeed(int speed)
+        {
+            this.speed = speed;
+        }
+        public void shoot()
+        {
+            Raylib.DrawText(sprite, GetPosX()+45, GetPosY(), 40, Color.Green);
+        }
+        public void DrawBullet()
+        {
+            Raylib.DrawText(sprite,GetPosX()+45,GetPosY(),40,Color.Green);
+        }
+        public void UpdatePosition()
+        {
+            posX = GetPosX();
+            posY = GetPosY() - speed;
+        }
+        public int GetPosX()
+        { return posX; }
+
+        public int GetPosY() { return posY; }
+
+    }
+
+
+
+    class Enemy : Character
+    {
+        public void MoveEnemey()
+        {
+            int EnemyX = GetPosX();
+            int EnemyY = GetPosY();
+            if(2800<=EnemyX)
+            {
+                UpdatePosition(50, EnemyY + 100);
+            }
+            else
+            {
+                UpdatePosition(EnemyX + GetSpeed(), EnemyY);
+            }
+        }
+        public bool KillPlayer(Player player)
+        {
+            int PlayerX = player.GetPosX();
+            int PlayerY = player.GetPosY();
+            int EnemyY = GetPosY();
+            int EnemyX = GetPosX();
+            if(Math.Abs(EnemyX-PlayerX)<15 && Math.Abs(PlayerY - EnemyY)<70)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ GUI.cs
@@ -0,0 +1,87 @@
+﻿using Raylib_cs;
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace Game
+{
+    class GUI
+    {
+        public static void Main(string[] args)
+        {
+            int TotalEnemies = 200;
+            List<Bullet> bullets = new List<Bullet>();
+            List<Enemy> enemies = new List<Enemy>();
+            string BGM = "RevenantFight.mp3";
+            Raylib.InitWindow(100, 100, "Space Invader");
+            int length = Raylib.GetMonitorHeight(0);
+            int width = Raylib.GetMonitorWidth(0);
+            int score = 0;
+            Raylib.CloseWindow();
+            Raylib.InitWindow(width, length, "Space Invader");
+            Raylib.SetWindowState(ConfigFlags.FullscreenMode);
+            Raylib.SetTargetFPS(90);
+            Raylib.InitAudioDevice();
+            Raylib.SetMasterVolume(1f);
+            Music BackgroundMusic = Raylib.LoadMusicStream(BGM);
+            //Raylib.PlayMusicStream(BackgroundMusic);
+            Player player = new Player();
+            Console.WriteLine($"LENGTH IS {length} AND WIDTH IS {width}");
+            player.InitPlayer(84, 40, 10, "space-invaders.png", (int)(width / 2), length - 100);
+            int EnemyStartX = 50;
+            int EnemeyStartY = (int)(length*1/4);
+            for (int i = 0;i<TotalEnemies;i++)
+            {
+                Enemy enemy = new Enemy();
+                enemy.InitPlayer(36, 40, 8, "game.png",((EnemyStartX+((i*36)/TotalEnemies))),EnemeyStartY);
+                enemy.MoveEnemey();
+                enemies.Add(enemy);
+            }
+            bool onMenu = true;
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+                if (Raylib.IsKeyPressed(KeyboardKey.Enter))
+                {
+                    onMenu = !onMenu;
+                }
+                if (onMenu)
+                {
+                    Raylib.DrawText($"Press enter to start the game!", (int)(width / 2 - 270), (int)(length / 2), 34, Color.White);
+                }
+                else
+                {
+                    if(Raylib.IsKeyPressed(KeyboardKey.Space))
+                    {
+                        Bullet bullet = new Bullet(player);
+                        bullet.shoot();
+                        bullets.Add(bullet);
+                    }
+                    foreach(Bullet bullet in bullets)
+                    {
+                        bullet.UpdatePosition();
+                        bullet.DrawBullet();
+                    }
+                    foreach (Enemy enemy in enemies){
+                        enemy.DrawCharacter();
+                        enemy.MoveEnemey();
+                        onMenu = (enemy.KillPlayer(player)) ? true : onMenu;
+                    }
+                    if (!Raylib.IsMusicStreamPlaying(BackgroundMusic))
+                    {
+                        Raylib.PlayMusicStream(BackgroundMusic);
+                    }
+                    player.DrawCharacter();
+                    player.HandleInput();
+                    Raylib.DrawText($"Score: {score}", 10, 40, 34, Color.White);
+                    Raylib.DrawText($"FPS: {(int)Raylib.GetFPS()}", width - 160, 40, 34, Color.White);
+                    Raylib.UpdateMusicStream(BackgroundMusic);
+                }
+                Raylib.EndDrawing();
+
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
